cc1bb868fac5fa074274bfa5999d7af3a207206a
Marko Kriška <marko.kriska@pm.me>
2022-06-23 13:29:09 +0200

    fish

---
+++ .config/fish/config.fish
@@ -0,0 +1,26 @@
+# path
+fish_add_path -m ~/.local/bin
+
+# export 
+set fish_greeting
+set TERM "xterm-256color"
+
+# aliases 
+alias vim="nvim"
+alias ariav="aria2c -d Videos"
+alias ariad="aria2c -d Downloads"
+alias pacl="expac --timefmt='%Y-%m-%d %T' '%l\t%n' | sort | tail -n 20"
+alias ls="exa -al --color=always --group-directories-first"
+alias mirror="sudo reflector -f 30 -l 30 --number 10 --verbose --save /etc/pacman.d/mirrorlist"
+alias mirrorcro="sudo reflector --verbose --country 'Croatia' -l 5 --sort rate --save /etc/pacman.d/mirrorlist"
+alias aptup="sudo apt update && sudo apt upgrade"
+alias aptupd="sudo apt update"
+alias aptupg="sudo apt upgrade"
+alias aptin="sudo apt install"
+alias aptrm="sudo apt remove"
+alias aptsr="sudo apt search"
+alias image="kitty +kitten icat"	# needs imagemagick
+
+# autostart
+colorscript random
+starship init fish | source
