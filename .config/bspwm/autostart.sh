3753d05838cdd7a56d1140be92be950d6b11a9a2
Marko Kriska <marko.kriska@pm.me>
2022-12-24 22:48:58 +0100

    update

--- .config/bspwm/autostart.sh
+++ .config/bspwm/autostart.sh
@@ -1,3 +1,5 @@
 xrandr --output eDP1 --mode 1366x768 --pos 1920x0 --output HDMI2 --primary --mode 1920x1080 --pos 0x0
+xrandr --output HDMI2 --set "Broadcast RGB" "Full"
 xsetroot -cursor_name left_ptr
+xbanish &
 sxhkd &
