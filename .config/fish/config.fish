7f039bc08d0d740317114571fef7ef68aec313a8
Marko Kriska <marko.kriska@pm.me>
2025-09-29 22:53:48 +0200

    update

--- .config/fish/config.fish
+++ .config/fish/config.fish
@@ -7,23 +7,9 @@ export PAGER="most"
 ### AUTOSTART 
 starship init fish | source
 atuin init fish | source
-#setxkbmap -option ctrl:nocaps
 
 ### PATH 
 fish_add_path -m $HOME/.local/bin
 
-### ALIASES 
-alias ariad="aria2c -d Downloads"
-alias ariam="aria2c -d Music"
-alias ariav="aria2c -d Videos"
-alias anime="ani-cli -q best"
-alias yta="yt-dlp --extract-audio --audio-format mp3 --audio-quality 0"
-alias ls="exa -al --color=always --group-directories-first"
-alias aptup="sudo apt update && sudo apt upgrade"
-alias aptin="sudo apt install"
-alias aptrm="sudo apt remove"
-alias aptsr="sudo apt search"
-alias image="sxiv"
-alias img="kitty +kitten icat"
-alias off="systemctl poweroff"
-alias reboot="systemctl reboot"
+### EZA 
+alias ls="eza -al --color=always --group-directories-first"
